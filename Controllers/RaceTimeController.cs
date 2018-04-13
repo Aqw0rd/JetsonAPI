e6223aa69cce798c7689f1e6c776d8081264e604
Vebjørn Nystad <vebjon15@student.uia.no>
2018-04-14 00:53:13 +0200

    Added extra routing.

--- Controllers/RaceTimeController.cs
+++ Controllers/RaceTimeController.cs
@@ -8,7 +8,7 @@ using System.Data.SqlClient;
 
 namespace JetsonAPI.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/[controller]/[action]")]
     public class RaceTimeController : Controller
     {
         private SqlCommand cmd;
